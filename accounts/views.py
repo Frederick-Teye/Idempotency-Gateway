29940007ffd3e0f17f686d2fdbda0ebb78e994e6
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2026-03-29 17:40:02 +0000

    Create UserRegistrationView to handle new user signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from .serializers import UserRegistrationSerializer
 
-# Create your views here.
+User = get_user_model()
+
+
+class UserRegistrationView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user_object = serializer.save()
+            token, created = Token.objects.get_or_create(user=user_object)
+            return Response(
+                {"user": serializer.data, "token": token.key},
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
