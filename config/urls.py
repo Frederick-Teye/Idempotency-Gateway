dc2bf5565286e58abca8c077754c6a3e9ee6fa60
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2026-03-29 22:33:02 +0000

    Include payment gateway url in config urls

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v1/process-payment/", include("payment_gateway.urls")),
     path("api/v1/auth/", include("accounts.urls")),
 ]
