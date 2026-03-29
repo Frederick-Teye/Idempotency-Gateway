51059668f268c2a080a45ca0afc7f2b757ca14af
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2026-03-29 14:15:10 +0000

    Add admin interface for the custom email-based User model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from accounts.models import CustomUser
+from .forms import (
+    CustomUserCreationForm,
+    CustomUserChangeForm,
+)
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    ordering = ("email",)
+    list_display = ("email", "is_staff", "is_active")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2", "is_staff", "is_active"),
+            },
+        ),
+    )
+
+    search_fields = ("email",)
