a935de45e823d2d077513a86068ed3818f926fc0
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2026-03-29 19:44:49 +0000

    Change endpoint signup/ to register/

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import UserRegistrationView, UserLoginView
 
 urlpatterns = [
-    path("signup/", UserRegistrationView.as_view(), name="api_signup"),
+    path("register/", UserRegistrationView.as_view(), name="api_register"),
     path("login/", UserLoginView.as_view(), name="api_login"),
 ]
