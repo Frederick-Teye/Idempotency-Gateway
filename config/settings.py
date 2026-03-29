2218efe52fe264342cf46600f9b2217b043a965f
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2026-03-29 22:22:58 +0000

    Add session authentication to default authentication classes

--- config/settings.py
+++ config/settings.py
@@ -127,5 +127,6 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
 }
