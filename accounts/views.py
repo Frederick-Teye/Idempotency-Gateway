f98654f9e81b1635fea771caf2ecc70d17949a07
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2026-03-29 17:41:30 +0000

    Create UserLoginView for signed in users to get token

--- accounts/views.py
+++ accounts/views.py
@@ -25,4 +25,19 @@ class UserRegistrationView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class UserLoginView(APIView):
+    permission_classes = (AllowAny,)
 
+    def post(self, request):
+        email = request.data.get("email")
+        password = request.data.get("password")
+        user = authenticate(username=email, password=password)
+
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({"token": token.key}, status=status.HTTP_200_OK)
+        else:
+            return Response(
+                {"error": "Invalid email or password"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
