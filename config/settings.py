dec9f1d60a8e997e95513eff3a38ef79c8b86d71
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2026-03-30 06:45:19 +0000

    Fix a typo

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="", cast=Csv())
+ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS", default="", cast=Csv())
 
 
 # Application definition
